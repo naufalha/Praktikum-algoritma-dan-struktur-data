8623da8d6fd9efa66d5ea3b94d8e4f96b09e5c0f
l200210135 <l200210135@student.ums.ac.id>
2023-04-16 19:24:14 +0700

    week7

---
+++ modul5/pertemuan7/week7.py
@@ -0,0 +1,18 @@
+##no 1 mencari index dari suatu angka dalam list dengan fungsi
+##searchnumber("sebuah list", "angka yang dicari")
+def searchnumbe(list, num):
+    for i in range(len(list)):
+        if num == list[i]:
+            return i
+    return -1
+
+
+
+##no 2 mencari titik tertinggi dari sebuah list yang tidak urut
+def hilltop(list):
+    highest = 0
+    for i in range(len(list)):
+        if list[i] > highest:
+            highest = i 
+    return highest
+print(hilltop([1,2,3,4,5,6,7,8,9,10,21,3,2,1,23,3,3,3,5]))
