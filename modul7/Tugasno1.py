6d981a96b2dcb14d7a6238cb9cb79d3ddecee248
l200210135 <l200210135@student.ums.ac.id>
2023-05-28 23:29:25 +0700

    1

--- modul7/Tugasno1.py
+++ modul7/Tugasno1.py
@@ -2,7 +2,8 @@ import re
 file = open("indonesia.txt", "r", encoding = 'latin1')
 teks = file.read()
 file.close()
-p = r"me\w+"
+p = r"di\w+"
 string = re.findall(p, teks)
-print("No. 1")
-print(string)
+for i in range(len(string)):
+    if len(string[i]) > 3:
+        print(string[i])
