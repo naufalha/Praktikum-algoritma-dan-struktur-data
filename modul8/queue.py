9e550b32e5d7d52c965de463a1058b2006a2ed15
naufalha <naufalha48@gmail.com>
2023-05-30 10:51:09 +0700

    queue

--- modul8/queue.py
+++ modul8/queue.py
@@ -5,3 +5,8 @@ class queue():
         print(len(self.item) == 0)
     def __len__(self):
         return len(self.item)
+    def enqueue(self, data):
+        self.item.append(data)  
+    def dequeue(self):
+        assert not self.isEmpty(), "Antrian sedang kosong"
+        return self.item.pop(0)
