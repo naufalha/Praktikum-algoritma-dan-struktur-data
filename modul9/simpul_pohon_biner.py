ea383ca0544dc7bdcd5529b33fdb4fece89159f6
naufalha <naufalha48@gmail.com>
2023-06-06 10:15:36 +0700

    pohon binner

---
+++ modul9/simpul_pohon_biner.py
@@ -0,0 +1,24 @@
+class _SimpulPohonBiner(object):
+    def __init__(self, data):
+        self.data = data
+        self.kiri = None
+        self.kanan = None
+        
+# membuat simpul dan mengisi data
+A = _SimpulPohonBiner('Ambarawa')
+B = _SimpulPohonBiner('Bantul')
+C = _SimpulPohonBiner('Cimahi')
+D = _SimpulPohonBiner('Denpasar')
+E = _SimpulPohonBiner('Enrekang')
+F = _SimpulPohonBiner('Flores')
+G = _SimpulPohonBiner('Garut')
+H = _SimpulPohonBiner('Halmahera Timur')
+I = _SimpulPohonBiner('Indramayu')
+J = _SimpulPohonBiner('Jakarta')
+
+# menghubungkan simpul
+A.kiri = B; A.kanan = C
+B.kiri = D; B.kanan = E
+C.kiri = F; C.kanan = G
+E.kiri = H
+G.kanan = I
